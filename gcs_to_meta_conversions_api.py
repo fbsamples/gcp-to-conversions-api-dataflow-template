392dc929f4ae53f2b6805a0ea4b5c45f8add951d
djememahamat <djeme.mahamat@gmail.com>
2024-07-03 13:41:44 +0100

    Update gcs_to_meta_conversions_api.py

--- gcs_to_meta_conversions_api.py
+++ gcs_to_meta_conversions_api.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+#  Copyright (c) Meta Platforms, Inc. and affiliates.
+#  All rights reserved.
+#  This source code is licensed under the license found in the
+#  LICENSE file in the root directory of this source tree.
 
 #  Google Cloud to Meta Conversions API Dataflow connector
 import apache_beam as beam
